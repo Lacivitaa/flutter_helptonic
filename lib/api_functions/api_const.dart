3f9cfe64b4074417a6bb1a804778ae7a04eac3d5
Lacivitaa <igor.lacivita@hotmail.com>
2021-03-25 22:30:05 -0300

    Api repository and connection (incomplete)

---
+++ lib/api_functions/api_const.dart
@@ -0,0 +1,6 @@
+class ApiConsts {
+  static const String apiBase = "s";
+  static const String apiVersion = 'v1';
+  static const String apiUrl = '$apiBase/$apiVersion';
+  static const String apiKey = '';
+}
